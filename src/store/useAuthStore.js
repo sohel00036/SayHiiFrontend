41f1834a2d293dbaa528c56f71c2c687779d168a
Sohel Hussain <sahilmansuri0036@gmail.com>
2026-09-20 16:46:46 +0530

    Update backend CORS and cookie settings for production

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -5,7 +5,7 @@ import { io } from "socket.io-client";
 
 const BASE_URL =
   import.meta.env.VITE_API_URL ||
-  (import.meta.env.MODE === "development" ? "http://localhost:3000" : "https://sayhiibackend.onrender.com");
+  (import.meta.env.MODE === "development" ? "http://localhost:3000" : "https://sayhiibackend-ezgs.onrender.com/");
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
